3a9e3c35537764a4ad877a8cb6510c955f3aaaff
Alex Yashin <kopninsky@gmail.com>
2021-10-13 03:22:47 +0300

    callbacks

--- index.js
+++ index.js
@@ -6,93 +6,79 @@ const entry = scriptArs[0] || 'input'
 const output = scriptArs[1] || 'output'
 const remove = scriptArs[2] || false
 
-const files = []
-let childDirs = []
+const entryPath = path.join(__dirname, entry)
+const outputPath = path.join(__dirname, output)
+
+
+const errorHandler = (err) => {
+  if (err) console.log(err)
+}
+
+const makeDirect = (filePath) => {
+  if (!fs.existsSync(filePath)) fs.mkdirSync(filePath)
+}
+
+const copyFile = (file) => {
+  const firstLetter = file.name[0].toUpperCase()
+  // not right
+  const newPath = path.join(outputPath, firstLetter)
+
+  makeDirect(newPath)
+
+  fs.copyFile(
+    file.path,
+    path.join(newPath, file.name),
+    err => errorHandler(err)
+  )
+}
+
+const reader = (filePath) => {
+  fs.readdir(filePath, (err, data) => {
+    errorHandler(err)
+    data.forEach((file) => {
+      const basePath = path.join(filePath, file)
+      const stat = fs.statSync(basePath)
+      if (stat.isFile()) {
+        copyFile({
+          name: file,
+          path: basePath
+        });
+      } else {
+        reader(basePath)
+      }
+    })
+  })
+}
+
+const deleter = (filePath) => {
+  fs.readdir(filePath, (err, data) => {
+      errorHandler(err)
+      data.forEach((file) => {
+        const basePath = path.join(filePath, file)
+        const stat = fs.statSync(basePath)
+        if (stat.isFile()) {
+          fs.unlinkSync(basePath)
+        } else {
+          deleter(basePath)
+        }
+      })
+  })
+}
 
 const main = (
-  readDir,
-  checkDir,
-  copyFiles,
-  removeDir,
-  close
-) => {
-  readDir()
-  checkDir()
-  copyFiles()
-  removeDir()
+  makeDirect,
+  reader,
+  deleter
+  ) => {
+  makeDirect(),
+  reader(),
+  deleter()
 }
 
-const readDir = (dir) => {
-  const dirs = fs.readdirSync(dir)
-  childDirs = dirs.slice()
-}
-
-const checkDir = (dir, childDirs) => {
-  childDirs.forEach(childDir => {
-    const directory = path.join(dir, childDir)
-
-    if (fs.statSync(directory).isDirectory()) {
-      const items = fs.readdirSync(directory)
-      return checkDir(directory, items)
-    }
-    return files.push({ name: childDir, path: dir })
-  })
-  return files
-}
-
-const createDir = (dir) => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(path.join(dir))
-    console.log(`Directory: ${dir} create successfully!`)
-  }
-}
-
-const copyFiles = (files, dir) => {
-  files.forEach(file => {
-    createDir(dir)
-    createDir(path.join(dir, file.name[0]))
-    const entry = path.join(file.path, file.name)
-    const output = path.join(dir, file.name[0], file.name)
-    fs.copyFileSync(entry, output)
-    console.log(`File: ${output} copied and sorted!`)
-  });
-}
-
-const removeDir = (dir) => {
-  let files
-  const rmSelf = true
-  dir = dir + '/';
-
-  try {
-    files = fs.readdirSync(dir)
-  } catch (e) { 
-    console.log(`Directory is not exist!`)
-    return
-  }
-
-  if (files.length > 0) {
-    files.forEach((x) => {
-      if (fs.statSync(dir + x).isDirectory()) {
-        removeDir(dir + x);
-      } else {
-        fs.unlinkSync(dir + x);
-      }
-    });
-  }
-
-  if (rmSelf) {
-    fs.rmdirSync(dir)
-  }
-
-  console.log(`Directory: ${dir} removed!`)
-}
-
-main(() => {
-  readDir(entry)
-}, () => {
-  checkDir(entry, childDirs)
-}, () => {
-  copyFiles(files, output)
-}, () => {
-  if (remove) removeDir(entry)
-})
+main(
+  () => makeDirect(outputPath),
+  () => reader(entryPath),
+  () => {
+    if (remove) deleter(entryPath)
+  },
+)
